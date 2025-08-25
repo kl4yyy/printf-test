1d5d76c0f100b93b077c29b2a7e84333b59a4cfc
kl4y <103466413+kl4yyy@users.noreply.github.com>
2025-08-25 03:10:32 +0000

    Create ft_printf.c

---
+++ ft_printf.c
@@ -0,0 +1,47 @@
+#include "ft_printf.h"
+
+static int ft_selc(char c , va_list args)
+{
+    int	count;
+
+	count = 0;
+	if (c == 'c')
+    count += ft_putchar(va_arg(args, char));
+	if (c == 's')
+    count += ft_putstr(va_arg(args, char *));
+	if (c == 'd' || c == 'i')
+    count += ft_putnbr(va_arg(args, int));
+	if (c == 'u')
+    count += ft_uns(va_arg(args, int));
+	if (c == 'p')
+    count += ft_ptr(va_arg(args, unsigned long));
+	if (c == 'x' || c == 'X')
+    count += ft_x(va_arg(args, unsigned int), c);
+	if (c == '%')
+	{
+		write(1, "%", 1);
+		count++;
+	}
+	return (count);
+}
+
+int ft_printf(const char *s, ...)
+{
+    va_list args;
+    int count;
+    int i;
+
+    count = 0;
+    i = 0;
+    va_start(args,s);
+    while(s[i])
+    {
+        if(s[i]=='%')
+            count+= ft_selc(s[++i],args);
+        else
+            count+=ft_putchar(s[i],1);
+        i++;
+    }
+    va_end(args);
+    return (count);
+}
